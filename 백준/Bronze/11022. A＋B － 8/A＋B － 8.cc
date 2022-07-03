5d413861b6498f8d9074e0ce48ee87285193516c
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-07-03 21:11:27 +0900

    [Bronze III] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t, a, b, i;
+    scanf("%d", &t);
+    for(i=0;i<t;i++) {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i+1, a, b, a+b);
+    }
+    return 0;
+}
