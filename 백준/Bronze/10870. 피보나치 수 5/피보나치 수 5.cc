c983b53b84603b63e8ec43fb3fb0dcf4e752abf3
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-07-20 02:13:37 +0900

    [Bronze V] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int fibonacci(int n) {
+    if(n>=2) {
+        n = fibonacci(n-1) + fibonacci(n-2);
+        return n;
+    }
+    else return n;
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", fibonacci(n));
+    return 0;
+}
