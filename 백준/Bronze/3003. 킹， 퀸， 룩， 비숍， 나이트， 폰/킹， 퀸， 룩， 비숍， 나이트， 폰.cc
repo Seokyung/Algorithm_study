2f47dcf73d01b690f10bbca6c9fb26bacba51d55
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-08-09 19:59:24 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int k,q,l,b,n,p;
+    cin>>k>>q>>l>>b>>n>>p;
+    cout<<1-k<<" "<<1-q<<" "<<2-l<<" "<<2-b<<" "<<2-n<<" "<<8-p<<"\n";
+    return 0;
+}
