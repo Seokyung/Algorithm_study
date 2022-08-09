452970b9886dbaef1d3407f9ea1481db62e8951a
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-08-09 19:31:15 +0900

    Update 나이순 정렬.cc

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -6,6 +6,10 @@ bool compare(pair<int, string> a, pair<int, string> b) {
     return a.first < b.first;
 }
 int main() {
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    
     int n, i;
     vector <pair<int, string>> member;
     cin>>n;
