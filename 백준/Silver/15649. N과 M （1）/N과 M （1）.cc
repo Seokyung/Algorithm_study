e68c14f5c07391f9153f24018615196cb2252954
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-10-09 16:33:23 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,49 @@
+//15649 N과 M(1)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define MAX 9
+
+int n, m;
+int result[MAX];
+bool visited[MAX];
+
+void print(int res[]) {
+    for(int i=0;i<m;i++) {
+        cout<<res[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+void permutation(int cnt) {
+    if(cnt == m) {
+        print(result);
+        return;
+    }
+    for(int i=1;i<=n;i++) {
+        if(visited[i] == true) {
+            continue;
+        }
+        else {
+            visited[i] = true;
+            result[cnt] = i;
+
+            permutation(cnt+1);
+
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin>>n>>m;
+
+    permutation(0);
+
+    return 0;
+}
