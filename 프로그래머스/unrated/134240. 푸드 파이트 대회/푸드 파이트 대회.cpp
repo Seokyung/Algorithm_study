c4f8d943ecc08a313e00a296bb4d6c702488dd84
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2023-02-05 22:38:17 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -1,26 +1,23 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 string solution(vector<int> food) {
     string answer = "";
-    vector<string> food_arr;
-    food_arr.resize(food.size());
     
     for(int i=1;i<food.size();i++) {
         for(int j=0;j<food[i]/2;j++) {
-            food_arr[i] += to_string(i);
+            answer += to_string(i);
         }
     }
     
-    for(int i=0;i<food_arr.size();i++) {
-        answer += food_arr[i];
-    }
-    answer += "0";
-    for(int i=food_arr.size()-1;i>=0;i--) {
-        answer += food_arr[i];
-    }
+    string tmp = answer;
+    
+    reverse(tmp.begin(), tmp.end());
+    
+    answer += "0" + tmp;
     
     return answer;
 }
