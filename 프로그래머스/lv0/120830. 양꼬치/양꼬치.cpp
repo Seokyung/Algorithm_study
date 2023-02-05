bab9335ebe0fc629cf9ce101be0db8578c9a4844
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2023-02-05 23:11:35 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.cpp
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int solution(int n, int k) {
     int answer = 0;
     
-    answer = (n*12000) + (k*2000) - ((n/10)*2000);
+    answer = (n*12000) + ((k - (n/10))*2000);
     
     return answer;
 }
