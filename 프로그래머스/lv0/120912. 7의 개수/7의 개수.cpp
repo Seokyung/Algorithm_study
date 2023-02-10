5c47af155b553d487b056fdaa3e32288e2788447
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2023-02-10 12:29:43 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array) {
+    int answer = 0;
+    
+    for(int i=0;i<array.size();i++) {
+        int num = array[i];
+        
+        while(num != 0) {
+            if(num%10 == 7) {
+                answer++;
+            }
+            num /= 10;
+        }
+    }
+    
+    return answer;
+}
