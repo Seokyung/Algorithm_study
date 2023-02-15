990662de2c3070d841bc6f453e07b20c7ab3d821
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2023-02-15 16:18:16 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,9 +1,3 @@
 function solution(numbers) {
-    var answer = 0;
-    
-    for(let i=0;i<numbers.length;i++) {
-        answer+=numbers.at(i);
-    }
-    
-    return answer/numbers.length;
+    return numbers.reduce((acc, cur) => acc + cur) / numbers.length;
 }
