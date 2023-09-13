d3a0ba304019daa3bde802cd8ba5dc1ec0a9d024
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2023-09-13 20:10:29 +0900

    [level 1] Title: 모의고사, Time: 4.76 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -0,0 +1,34 @@
+function solution(answers) {
+    let result = [];
+    let std1 = [1, 2, 3, 4, 5], std2 = [2, 1, 2, 3, 2, 4, 2, 5], std3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let std1_res = 0, std2_res = 0, std3_res = 0;
+    
+    for(let i = 0; i < answers.length; i++) {
+        if(answers[i] === std1[i%5]) {
+            std1_res++;
+        }
+        if(answers[i] === std2[i%8]) {
+            std2_res++;
+        }
+        if(answers[i] === std3[i%10]) {
+            std3_res++;
+        }
+    }
+    
+    let max = std1_res > std2_res ? 
+        (std1_res > std3_res ? std1_res : std3_res)
+         : 
+         (std2_res > std3_res ? std2_res : std3_res);
+    
+    if(max === std1_res) {
+        result.push(1);
+    }
+    if(max === std2_res) {
+        result.push(2);
+    }
+    if(max === std3_res) {
+        result.push(3);
+    }
+    
+    return result;
+}
