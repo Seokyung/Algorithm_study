39e5059a7e397c272f7c5c570f92bf6be3814637
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2024-09-15 16:15:43 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,24 @@
+function solution(n) {
+    let answer = 0;
+    
+    for (let i = 1; i <= n; i++) {
+        answer += 1;
+        
+        if (i % 3 === 0 ||
+            i % 10 === 3 ||
+            Math.floor(i / 10) === 3 ||
+            answer % 3 === 0 ||
+            answer % 10 === 3 ||
+            Math.floor((answer % 100) / 10) === 3
+           ) {
+            while (answer % 3 === 0 ||
+                   answer % 10 === 3 ||
+                   Math.floor((answer % 100) / 10) === 3
+                  ) {
+                answer += 1;
+            }
+        }
+    }
+    
+    return answer;
+}
