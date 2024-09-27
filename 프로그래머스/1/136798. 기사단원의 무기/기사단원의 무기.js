bbda8e300291eb9346414eb8d8455685d0e5d9f7
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2024-09-27 17:09:59 +0900

    [level 1] Title: 기사단원의 무기, Time: 239.80 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,21 +1,27 @@
+const getFactorCnt = (n) => {
+    if (n === 1) {
+        return 1;
+    } else {
+        let factorSet = new Set();
+        let d = Math.ceil(Math.sqrt(n));
+
+        while (d > 0) {
+            if (n % d === 0) {
+                factorSet.add(d);
+                factorSet.add(n / d);
+            }
+            d--;
+        }
+        
+        return factorSet.size;
+    }
+}
+
 function solution(number, limit, power) {
     let weight = 0;
     
     for (let i = 1; i <= number; i++) {
-        let factorCnt = 1;
-        
-        if (i === 1) {
-            factorCnt = 1;
-        } else {
-            let d = Math.ceil(i / 2);
-            
-            while ((d > 0) && (factorCnt <= limit)) {
-                if (i % d === 0) {
-                    factorCnt++;
-                }
-                d--;
-            }
-        }
+        const factorCnt = getFactorCnt(i);
         
         if (factorCnt > limit) {
             weight += power;
