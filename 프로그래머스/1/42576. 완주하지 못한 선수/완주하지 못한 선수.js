cbe5ed5cdfdfdbef7fd89e1e2e0f4e940f47c4a0
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2024-10-04 07:03:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 112.27 ms, Memory: 58.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,13 @@
+function solution(participant, completion) {
+    const sortedParticipant = participant.sort();
+    const sortedCompletion = completion.sort();
+    
+    for (let i = 0; i < sortedParticipant.length; i++) {
+        if (sortedParticipant[i] !== sortedCompletion[i]) {
+            answer = sortedParticipant[i];
+            break;
+        }
+    }
+    
+    return answer;
+}
