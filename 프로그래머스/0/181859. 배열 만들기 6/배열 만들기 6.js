8939c5cb09a5bc5d6f6abcfd532cc312e5aa4d6e
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2024-10-16 11:44:04 +0900

    [level 0] Title: 배열 만들기 6, Time: 19.08 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -1,15 +1,13 @@
 function solution(arr) {
     let answer = [];
-    let i = 0;
     
-    while (i < arr.length) {
-        if ((answer.length !== 0) && (answer[answer.length - 1] === arr[i])) {
+    arr.forEach((el) => {
+        if ((answer.length !== 0) && (answer[answer.length - 1] === el)) {
             answer.pop();
         } else {
-            answer.push(arr[i]);
+            answer.push(el);
         }
-        i++;
-    }
+    })
     
     return answer.length === 0 ? [-1] : answer;
 }
