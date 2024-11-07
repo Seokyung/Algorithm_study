004070472a75389ece75dec9fc2930fd3987a3f9
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2024-11-07 16:50:26 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,3 +1,6 @@
 function solution(my_string, is_suffix) {
+    // slice() 메서드 사용
     return (my_string.slice(0 - is_suffix.length) === is_suffix) & 1;
+    // endsWith() 메서드 사용
+    // return my_string.endsWith(is_suffix) & 1;
 }
