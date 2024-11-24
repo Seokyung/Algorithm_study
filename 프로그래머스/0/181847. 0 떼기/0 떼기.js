d8408b7cdddf8f6414fcd9f1fc1ac3c627a37b6d
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2024-11-24 23:59:59 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -1,9 +1,13 @@
 function solution(n_str) {
-    const str = [...n_str];
+    // while문 사용
+//     const str = [...n_str];
     
-    while (str[0] === '0') {
-        str.shift();
-    }
+//     while (str[0] === '0') {
+//         str.shift();
+//     }
 
-    return str.join('');
+//     return str.join('');
+    
+    // 문자열을 숫자로 변경
+    return Number(n_str) + '';
 }
