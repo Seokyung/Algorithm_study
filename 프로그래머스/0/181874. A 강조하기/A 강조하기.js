71b50cd7b5cd61a06a119d250458eecaa3bde72a
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2024-12-06 14:31:53 +0900

    [level 0] Title: A 강조하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,9 @@
+function solution(myString) {
+    return [...myString].map((el) => {
+        if (el === 'a' || el === 'A') {
+            return 'A';
+        } else {
+            return el.toLowerCase();
+        }
+    }).join('');
+}
