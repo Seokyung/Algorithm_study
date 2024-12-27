2572c248d20790721d4db498a07a3891de5faddc
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2024-12-28 01:28:55 +0900

    [level 0] Title: 문자열 묶기, Time: 11.81 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -1,5 +1,5 @@
 function solution(strArr) {
     const arr = Array.from({length: 31}, () => 0);
     strArr.forEach((el) => arr[el.length]++);
-    return arr.sort((a, b) => b - a)[0];
+    return Math.max(...arr);
 }
